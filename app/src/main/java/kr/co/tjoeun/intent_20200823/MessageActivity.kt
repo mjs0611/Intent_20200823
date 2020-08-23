62a1e0e3b73bef272c5368840b90efe6274709a8
JunSeung Mo <mojuns@gmail.com>
2020-08-23 16:20:35 +0900

    전달 받은 메세지 출력 예제

--- app/src/main/java/kr/co/tjoeun/intent_20200823/MessageActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200823/MessageActivity.kt
@@ -2,10 +2,17 @@ package kr.co.tjoeun.intent_20200823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
+
+//        화면이 만들어 질 때 => 첨부된 데이터를 받아서 => 텍스트뷰에 반영
+        val receivedMessage = intent.getStringExtra("message")
+
+        messageTxt.text = receivedMessage
+
     }
 }
