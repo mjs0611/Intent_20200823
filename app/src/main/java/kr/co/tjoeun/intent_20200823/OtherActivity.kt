d5ea68fbf81a604750a3dde55e396a01c10c6bcc
JunSeung Mo <mojuns@gmail.com>
2020-08-23 16:03:43 +0900

    다른화면에서 복귀버튼 누르면 => finish() 처리로 화면 종료 (자동 복귀)

--- app/src/main/java/kr/co/tjoeun/intent_20200823/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200823/OtherActivity.kt
@@ -1,11 +1,24 @@
 package kr.co.tjoeun.intent_20200823
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+
+        }
+
     }
+
 }
