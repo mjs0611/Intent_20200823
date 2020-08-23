2c643ca9f55e9861963593c9b2eca69f1cbab21e
JunSeung Mo <mojuns@gmail.com>
2020-08-23 15:53:55 +0900

    메인화면 -> 버튼 눌리면 -> 다른화면으로 이동 예제

--- app/src/main/java/kr/co/tjoeun/intent_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200823/MainActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.tjoeun.intent_20200823
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        다른화면으로 이동하기 버튼이 눌리면
+        moveToOtherActivityBtn.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
